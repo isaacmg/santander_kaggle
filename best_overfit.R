48e5b0667a78992b5244e296b69b3f34b89c0112
Isaac Godfried <igodfried@isaac26.com>
2016-04-28 02:12:22 -0400

    recent

---
+++ best_overfit.R
@@ -0,0 +1,154 @@
+library(xgboost)
+library(Matrix)
+
+set.seed(1234)
+
+train <- read.csv("train.csv")
+test  <- read.csv("test.csv")
+
+##### Removing IDs
+train$ID <- NULL 
+test.id <- test$ID
+test$ID <- NULL
+
+##### Extracting TARGET
+train.y <- train$TARGET
+train$TARGET <- NULL
+count1 <- function(x) {
+  return( sum(x >1) )
+}
+n1 <- apply(test, 1, FUN=count1) 
+var15 = test['var15']
+saldo_medio_var5_hace2 = test['saldo_medio_var5_hace2']
+saldo_var33 = test['saldo_var33']
+var38 = test['var38']
+v21 = test['var21']
+nv = test['num_var33']+test['saldo_medio_var33_ult3']+test['saldo_medio_var44_hace2']+test['saldo_medio_var44_hace3']+
+test['saldo_medio_var33_ult1']+test['saldo_medio_var44_ult1']
+num_var30 = test['num_var30']
+num_var13_0 = test['num_var13_0']
+num_var33_0 = test['num_var33_0']
+
+
+# BAD
+# num_var35 = test['num_var35']
+# No improvement
+# num_var1 = test['num_var1']
+
+
+##### 0 count per line
+count0 <- function(x) {
+  return( sum(x == 0) )
+}
+train$n0 <- apply(train, 1, FUN=count0)
+test$n0 <- apply(test, 1, FUN=count0)
+
+##### Removing constant features
+cat("\n## Removing the constants features.\n")
+for (f in names(train)) {
+  if (length(unique(train[[f]])) == 1) {
+    cat(f, "is constant in train. We delete it.\n")
+    train[[f]] <- NULL
+    test[[f]] <- NULL
+  }
+}
+
+##### Removing identical features
+features_pair <- combn(names(train), 2, simplify = F)
+toRemove <- c()
+for(pair in features_pair) {
+  f1 <- pair[1]
+  f2 <- pair[2]
+  
+  if (!(f1 %in% toRemove) & !(f2 %in% toRemove)) {
+    if (all(train[[f1]] == train[[f2]])) {
+      cat(f1, "and", f2, "are equals.\n")
+      toRemove <- c(toRemove, f2)
+    }
+  }
+}
+
+feature.names <- setdiff(names(train), toRemove)
+
+train$var38 <- log(train$var38)
+test$var38 <- log(test$var38)
+
+train <- train[, feature.names]
+test <- test[, feature.names]
+
+#---limit vars in test based on min and max vals of train
+print('Setting min-max lims on test data')
+for(f in colnames(train)){
+  lim <- min(train[,f])
+  test[test[,f]<lim,f] <- lim
+  
+  lim <- max(train[,f])
+  test[test[,f]>lim,f] <- lim  
+}
+#---
+
+train$TARGET <- train.y
+
+
+train <- sparse.model.matrix(TARGET ~ ., data = train)
+
+dtrain <- xgb.DMatrix(data=train, label=train.y)
+watchlist <- list(train=dtrain)
+
+param <- list(  objective           = "binary:logistic", 
+                booster             = "gbtree",
+                eval_metric         = "auc",
+                eta                 = 0.0202047,
+                max_depth           = 5,
+                subsample           = 0.6815,
+                colsample_bytree    = 0.701
+)
+
+clf <- xgb.train(   params              = param, 
+                    data                = dtrain, 
+                    nrounds             = 560, 
+                    verbose             = 1,
+                    watchlist           = watchlist,
+                    maximize            = FALSE
+)
+
+
+#######actual variables
+
+feature.names
+
+test$TARGET <- -1
+
+test <- sparse.model.matrix(TARGET ~ ., data = test)
+
+preds <- predict(clf, test)
+pred <-predict(clf,train)
+AUC<-function(actual,predicted)
+{
+  library(pROC)
+  auc<-auc(as.numeric(actual),as.numeric(predicted))
+  auc 
+}
+AUC(train.y,pred) ##AUC
+
+preds[var15 < 23] = 0
+preds[saldo_medio_var5_hace2 > 160000] = 0
+preds[saldo_var33 > 0] = 0
+preds[var38 > 3988596] = 0
+preds[nv > 0] = 0
+preds[v21 > 7500] = 0
+preds[num_var30 > 9] = 0
+preds[num_var13_0 > 6] = 0
+preds[num_var33_0 > 0] = 0
+
+# BAD
+# preds[num_var35 > 21] = 0
+# preds[num_var1 > 3] = 0
+
+# Testing
+preds[preds<0.002]=0
+#result
+preds[n1>115]=0
+submission <- data.frame(ID=test.id, TARGET=preds)
+cat("saving the submission file\n")
+write.csv(submission, "submission.csv", row.names = F)
